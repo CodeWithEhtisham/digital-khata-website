da26f1e55f85a6f6db725b97f3d3f2d7c6d478c0
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-12-25 19:14:02 +0500

    main base added

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class SignInView(TemplateView):
-    template_name = "index.html"
+    template_name = "accounts/sign-in.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)

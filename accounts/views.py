cb402f5259a76177cd5d28522f2e70878a812f27
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-12-30 23:51:49 +0500

    code modified

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class SignInView(TemplateView):
-    template_name = "accounts/landing_page.html"
+    template_name = "accounts/sign-up.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)

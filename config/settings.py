01774888c9f7a36bff742a57b1855b99c4ca3ee1
CodeWithEhtisham <shamikhan0347@gmail.com>
2024-12-30 23:15:55 +0500

    setting updated

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',
     'accounts',
     'roznamcha',
 ]
@@ -122,11 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-COMPRESS_ROOT = BASE_DIR / 'static'
 
-COMPRESS_ENABLED = True
-
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 

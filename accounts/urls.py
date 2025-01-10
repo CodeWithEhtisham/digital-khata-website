311710bd305c61fc1e12490bd028de9dfd1f167c
CodeWithEhtisham <shamikhan0347@gmail.com>
2025-01-10 13:21:47 +0500

    code updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import SignInView
+from .views import SignInView,SignUpView
 urlpatterns = [
-    path('', SignInView.as_view(), name='signin'),
+    path('sign-in/', SignInView.as_view(), name='signin'),
+    path('sign-up/', SignUpView.as_view(), name='signup'),
 ]
